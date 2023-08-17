3fd79d788dc4a2232b4746f086e7366ce5a58c70
Laman Ismayilova <laman.mamedova98@gmail.com>
2023-08-17 11:47:25 -0700

    updated

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require ('express')
 const app = express()
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'ac2a65f5cb904005b01a0b49bbedc067',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.use(express.static(`${__dirname}/public`))
 app.get('/api/cat',(req,res)=>{
